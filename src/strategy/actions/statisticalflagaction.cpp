f56cc4ce12c110e47c46519502780bf88c292432
André Offringa <offringa@gmail.com>
2017-08-16 23:48:05 +0200

    Avoid extra calculations in StatisticalFlagging when tf/time/freq thresholds are not set

--- src/strategy/actions/statisticalflagaction.cpp
+++ src/strategy/actions/statisticalflagaction.cpp
@@ -19,32 +19,41 @@ namespace rfiStrategy {
 		SIROperator::OperateHorizontally(mask, _minimumGoodTimeRatio);
 		SIROperator::OperateVertically(mask, _minimumGoodFrequencyRatio);
 		
-		for(size_t y=0; y!=mask->Height(); ++y)
+		if(_minAvailableTimesRatio > 0)
 		{
-			bool* rowPtr = mask->ValuePtr(0, y);
-			
-			size_t count = 0;
-			for(size_t x=0; x!=mask->Width(); ++x)
-			{
-				if(rowPtr[x]) ++count;
-			}
-			if(count > mask->Width() * (1.0-_minAvailableTimesRatio))
-				mask->SetAllHorizontally<true>(y);
-		}
-		
-		for(size_t x=0; x!=mask->Width(); ++x)
-		{
-			size_t count = 0;
 			for(size_t y=0; y!=mask->Height(); ++y)
 			{
-				if(mask->Value(x, y)) ++count;
+				bool* rowPtr = mask->ValuePtr(0, y);
+				
+				size_t count = 0;
+				for(size_t x=0; x!=mask->Width(); ++x)
+				{
+					if(rowPtr[x]) ++count;
+				}
+				if(count > mask->Width() * (1.0-_minAvailableTimesRatio))
+					mask->SetAllHorizontally<true>(y);
 			}
-			if(count > mask->Height() * (1.0-_minAvailableFrequenciesRatio))
-				mask->SetAllVertically<true>(x);
 		}
 		
-		if(mask->GetCount<true>() > mask->Width()*mask->Height()*(1.0-_minAvailableTFRatio))
-			mask->SetAll<true>();
+		if(_minAvailableFrequenciesRatio > 0.0)
+		{
+			for(size_t x=0; x!=mask->Width(); ++x)
+			{
+				size_t count = 0;
+				for(size_t y=0; y!=mask->Height(); ++y)
+				{
+					if(mask->Value(x, y)) ++count;
+				}
+				if(count > mask->Height() * (1.0-_minAvailableFrequenciesRatio))
+					mask->SetAllVertically<true>(x);
+			}
+		}
+		
+		if(_minAvailableTFRatio > 0.0)
+		{
+			if(mask->GetCount<true>() > mask->Width()*mask->Height()*(1.0-_minAvailableTFRatio))
+				mask->SetAll<true>();
+		}
 		
 		data.SetGlobalMask(mask);
 	}
