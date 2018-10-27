e637a22265ddf22fe328f7f4b05dee0fe00781e3
André Offringa <offringa@gmail.com>
2018-10-27 11:18:30 +0200

    Add cubehelix colourmap

--- src/structures/colormap.cpp
+++ src/structures/colormap.cpp
@@ -35,11 +35,15 @@ std::unique_ptr<ColorMap> ColorMap::CreateColorMap(const std::string& type)
 		return UC(new ContrastMap("invpositive"));
 	else if(type == "viridis")
 		return UC(new ViridisMap());
+	else if(type == "cubehelix")
+		return UC(new CubeHelixMap());
+	else if(type == "cubehelixcolourful")
+		return UC(new CubeHelixColourfulMap());
 	else
 		return UC(new MonochromeMap());
 }
 
-const std::string ColorMap::_colorMapsString("monochrome, coldhot, redblue, redyellowblue, fire, contrast, coldhotcontrast, redbluecontrast, redyellowbluecontrast, positive, invpositive, positivecontrast, invpositivecontrast, viridis");
+const std::string ColorMap::_colorMapsString("monochrome, coldhot, redblue, redyellowblue, fire, contrast, coldhotcontrast, redbluecontrast, redyellowbluecontrast, positive, invpositive, positivecontrast, invpositivecontrast, cubehelix, cubehelixcolourful, viridis");
 
 const std::string &ColorMap::GetColorMapsString() throw()
 {
