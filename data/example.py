e9712338480deded66a91b5b3b1c419098cec894
André Offringa <offringa@gmail.com>
2019-05-17 20:42:54 +0200

    Implement Python wrapper to the C++ interface

---
+++ data/example.py
@@ -0,0 +1,62 @@
+import aoflagger as aof
+import numpy
+import sys
+
+print("Flagging with AOFlagger version " + aof.AOFlagger.get_version_string())
+
+nch = 256
+ntimes = 1000
+count=50       # number of trials in the false-positives test
+
+aoflagger = aof.AOFlagger()
+
+# Load strategy from disk (alternatively use 'make_strategy' to use a default one)
+strategy = aoflagger.load_strategy("example-strategy.rfis")
+
+data = aoflagger.make_image_set(ntimes, nch, 8)
+
+print("Number of times: " + str(data.width()))
+print("Number of channels: " + str(data.height()))
+
+# When flagging multiple baselines, iterate over the baselines and
+# call the following code for each baseline
+# (to use multithreading, make sure to create an imageset for each
+# thread)
+
+# Make eight images: real and imaginary for 4 pol
+
+for imgindex in range(8):
+    # Initialize data with random numbers
+    values = numpy.zeros([ntimes, nch])
+    data.set_image_buffer(imgindex, values)
+    
+flags = aoflagger.run(strategy, data)
+flagvalues = flags.get_buffer()
+flagcount = sum(sum(flagvalues))
+print("Percentage flags on zero data: " + str(flagcount * 100.0 / (nch*ntimes)) + "%")
+
+ratiosum=0.0
+ratiosumsq=0.0
+
+# This is a simple example to calculate the false-positive ratio
+# on Gaussian data.
+for repeat in range(count):
+    for imgindex in range(8):
+        # Initialize data with random numbers
+        values = numpy.random.normal(0, 1, [ntimes, nch])
+        data.set_image_buffer(imgindex, values)
+        
+    flags = aoflagger.run(strategy, data)
+    flagvalues = flags.get_buffer()
+    ratio = float(sum(sum(flagvalues))) / (nch*ntimes)
+    ratiosum += ratio
+    ratiosumsq += ratio*ratio
+    sys.stdout.write('.')
+    sys.stdout.flush()
+
+print('')
+    
+print("Percentage flags (false-positive rate) on Gaussian data: " +
+      str(ratiosum * 100.0 / count) + "% +/- " +
+      str(numpy.sqrt((ratiosumsq/count - ratiosum*ratiosum / (count*count))) * 100.0)
+     )
--- src/python/pyinterface.cpp
+++ src/python/pyinterface.cpp
@@ -1,13 +1,130 @@
 #include <boost/python.hpp>
- 
+
+#include <boost/python/numpy.hpp>
+
+#include <numpy/arrayobject.h>
+
 #include "data.h"
 #include "functions.h"
 
 #include "../structures/polarization.h"
- 
+
+#include "../interface/aoflagger.h"
+
+#include <iostream>
+
+static boost::python::numpy::ndarray GetImageBuffer(const aoflagger::ImageSet* imageSet, size_t imageIndex)
+{
+	namespace np = boost::python::numpy;
+	const float* values = imageSet->ImageBuffer(imageIndex);
+	Py_intptr_t shape[2] = { unsigned(imageSet->Width()), unsigned(imageSet->Height()) };
+	np::ndarray result = np::zeros(2, shape, np::dtype::get_builtin<double>());
+	std::copy_n(values, imageSet->Width()*imageSet->Height(), reinterpret_cast<double*>(result.get_data()));
+	return result;
+}
+
+static void SetImageBuffer(aoflagger::ImageSet* imageSet, size_t imageIndex, const boost::python::numpy::ndarray& values)
+{
+	namespace np = boost::python::numpy;
+	if(values.get_dtype() != np::dtype::get_builtin<double>())
+		throw std::runtime_error("ImageSet.set_image_buffer(): Invalid type specified for data array; double numpy array required");
+	if(values.shape(0) != imageSet->Width() || values.shape(1) != imageSet->Height())
+		throw std::runtime_error("ImageSet.set_image_buffer(): dimensions of provided array doesn't match with image set");
+	const double *data = reinterpret_cast<const double*>(values.get_data());
+	if(!data)
+		throw std::runtime_error("Data needs to be provided that is interpretable as a float array");
+	float* buffer = imageSet->ImageBuffer(imageIndex);
+	for(size_t y=0; y!=imageSet->Height(); ++y)
+	{
+		float* row = buffer + y * imageSet->HorizontalStride();
+		for(size_t x=0; x!=imageSet->Width(); ++x)
+		{
+			row[x] = *data;
+			++data;
+		}
+	}
+}
+
+static boost::python::numpy::ndarray GetBuffer(const aoflagger::FlagMask* flagMask)
+{
+	namespace np = boost::python::numpy;
+	const bool* values = flagMask->Buffer();
+	Py_intptr_t shape[2] = { unsigned(flagMask->Width()), unsigned(flagMask->Height()) };
+	np::ndarray result = np::zeros(2, shape, np::dtype::get_builtin<bool>());
+	std::copy_n(values, flagMask->Width()*flagMask->Height(), reinterpret_cast<bool*>(result.get_data()));
+	return result;
+}
+
+static void SetBuffer(aoflagger::FlagMask* flagMask, const boost::python::numpy::ndarray& values)
+{
+	namespace np = boost::python::numpy;
+	if(values.get_dtype() != np::dtype::get_builtin<bool>())
+		throw std::runtime_error("FlagMask.set_buffer(): Invalid type specified for data array; double numpy array required");
+	PyArrayObject *arr = reinterpret_cast<PyArrayObject*>(values.ptr());
+	if(PyArray_SHAPE(arr)[0] != flagMask->Width() || PyArray_SHAPE(arr)[1] != flagMask->Height())
+		throw std::runtime_error("FlagMask.set_buffer(): dimensions of provided array doesn't match with image set");
+	const bool *data = (bool *) PyArray_DATA(arr);
+	if(!data)
+		throw std::runtime_error("Data needs to be provided that is interpretable as a float array");
+	bool* buffer = flagMask->Buffer();
+	for(size_t y=0; y!=flagMask->Height(); ++y)
+	{
+		bool* row = buffer + y * flagMask->HorizontalStride();
+		for(size_t x=0; x!=flagMask->Width(); ++x)
+		{
+			row[x] = *data;
+			++data;
+		}
+	}
+}
+
+boost::python::object MakeImageSet1(aoflagger::AOFlagger* flagger, size_t width, size_t height, size_t count)
+{ return boost::python::object(flagger->MakeImageSet(width, height, count)); }
+
+boost::python::object MakeImageSet2(aoflagger::AOFlagger* flagger, size_t width, size_t height, size_t count, size_t widthCapacity)
+{ return boost::python::object(flagger->MakeImageSet(width, height, count, widthCapacity)); }
+
+boost::python::object MakeImageSet3(aoflagger::AOFlagger* flagger, size_t width, size_t height, size_t count, float initialValue)
+{ return boost::python::object(flagger->MakeImageSet(width, height, count, initialValue)); }
+
+boost::python::object MakeImageSet4(aoflagger::AOFlagger* flagger, size_t width, size_t height, size_t count, float initialValue, size_t widthCapacity)
+{ return boost::python::object(flagger->MakeImageSet(width, height, count, initialValue, widthCapacity)); }
+
+boost::python::object MakeFlagMask1(aoflagger::AOFlagger* flagger, size_t width, size_t height)
+{ return boost::python::object(flagger->MakeFlagMask(width, height)); }
+
+boost::python::object MakeFlagMask2(aoflagger::AOFlagger* flagger, size_t width, size_t height, bool initialValue)
+{ return boost::python::object(flagger->MakeFlagMask(width, height, initialValue)); }
+
+boost::python::object MakeStrategy1(aoflagger::AOFlagger* flagger, enum aoflagger::TelescopeId telescopeId)
+{ return boost::python::object(flagger->MakeStrategy(telescopeId)); }
+
+boost::python::object MakeStrategy2(aoflagger::AOFlagger* flagger, enum aoflagger::TelescopeId telescopeId, unsigned strategyFlags)
+{ return boost::python::object(flagger->MakeStrategy(telescopeId, strategyFlags)); }
+
+boost::python::object MakeStrategy3(aoflagger::AOFlagger* flagger, enum aoflagger::TelescopeId telescopeId, unsigned strategyFlags, double frequency)
+{ return boost::python::object(flagger->MakeStrategy(telescopeId, strategyFlags, frequency)); }
+
+boost::python::object MakeStrategy4(aoflagger::AOFlagger* flagger, enum aoflagger::TelescopeId telescopeId, unsigned strategyFlags, double frequency, double timeRes)
+{ return boost::python::object(flagger->MakeStrategy(telescopeId, strategyFlags, frequency, timeRes)); }
+
+boost::python::object MakeStrategy5(aoflagger::AOFlagger* flagger, enum aoflagger::TelescopeId telescopeId, unsigned strategyFlags, double frequency, double timeRes, double frequencyRes)
+{ return boost::python::object(flagger->MakeStrategy(telescopeId, strategyFlags, frequency, timeRes, frequencyRes)); }
+
+boost::python::object LoadStrategy(aoflagger::AOFlagger* flagger, const std::string& filename)
+{ return boost::python::object(flagger->LoadStrategy(filename)); }
+
+boost::python::object Run1(aoflagger::AOFlagger* flagger, aoflagger::Strategy& strategy, const aoflagger::ImageSet& input)
+{ return boost::python::object(flagger->Run(strategy, input)); }
+
+boost::python::object Run2(aoflagger::AOFlagger* flagger, aoflagger::Strategy& strategy, const aoflagger::ImageSet& input, const aoflagger::FlagMask& existingFlags)
+{ return boost::python::object(flagger->Run(strategy, input, existingFlags)); }
+
 BOOST_PYTHON_MODULE(aoflagger)
 {
 	using namespace boost::python;
+	
+	boost::python::numpy::initialize();
 	
 	class_<aoflagger_python::Data>("Data")
 		.def(self - self)
@@ -53,4 +170,66 @@ BOOST_PYTHON_MODULE(aoflagger)
 		.value("PhasePart", TimeFrequencyData::PhasePart)
 		.value("AmplitudePart", TimeFrequencyData::AmplitudePart)
 		.value("ComplexParts", TimeFrequencyData::ComplexParts);
+		
+	enum_<enum aoflagger::TelescopeId>("TelescopeId")
+		.value("Generic", aoflagger::GENERIC_TELESCOPE)
+		.value("AARTFAAC", aoflagger::AARTFAAC_TELESCOPE)
+		.value("Arecibo", aoflagger::ARECIBO_TELESCOPE)
+		.value("Bighorns", aoflagger::BIGHORNS_TELESCOPE)
+		.value("JVLA", aoflagger::JVLA_TELESCOPE)
+		.value("LOFAR", aoflagger::LOFAR_TELESCOPE)
+		.value("MWA", aoflagger::MWA_TELESCOPE)
+		.value("Parkes", aoflagger::PARKES_TELESCOPE)
+		.value("WSRT", aoflagger::WSRT_TELESCOPE);
+
+	class_<aoflagger::StrategyFlags>("StrategyFlags", no_init)
+		.def_readonly("NONE", &aoflagger::StrategyFlags::NONE)
+		.def_readonly("LARGE_BANDWIDTH", &aoflagger::StrategyFlags::LARGE_BANDWIDTH)
+		.def_readonly("SMALL_BANDWIDTH", &aoflagger::StrategyFlags::SMALL_BANDWIDTH)
+		.def_readonly("TRANSIENTS", &aoflagger::StrategyFlags::TRANSIENTS)
+		.def_readonly("ROBUST", &aoflagger::StrategyFlags::ROBUST)
+		.def_readonly("FAST", &aoflagger::StrategyFlags::FAST)
+		.def_readonly("INSENSITIVE", &aoflagger::StrategyFlags::INSENSITIVE)
+		.def_readonly("SENSITIVE", &aoflagger::StrategyFlags::SENSITIVE)
+		.def_readonly("USE_ORIGINAL_FLAGS", &aoflagger::StrategyFlags::USE_ORIGINAL_FLAGS)
+		.def_readonly("AUTO_CORRELATION", &aoflagger::StrategyFlags::AUTO_CORRELATION)
+		.def_readonly("HIGH_TIME_RESOLUTION", &aoflagger::StrategyFlags::HIGH_TIME_RESOLUTION);
+
+	class_<aoflagger::ImageSet>("ImageSet")
+		.def("width", &aoflagger::ImageSet::Width)
+		.def("height", &aoflagger::ImageSet::Height)
+		.def("image_count", &aoflagger::ImageSet::ImageCount)
+		.def("horizontal_stride", &aoflagger::ImageSet::HorizontalStride)
+		.def("set", &aoflagger::ImageSet::Set)
+		.def("get_image_buffer", GetImageBuffer)
+		.def("set_image_buffer", SetImageBuffer)
+		.def("resize_without_reallocation", &aoflagger::ImageSet::ResizeWithoutReallocation);
+	
+	class_<aoflagger::FlagMask>("FlagMask")
+		.def("width", &aoflagger::FlagMask::Width)
+		.def("height", &aoflagger::FlagMask::Height)
+		.def("horizontal_stride", &aoflagger::FlagMask::HorizontalStride)
+		.def("get_buffer", GetBuffer)
+		.def("set_buffer", SetBuffer)
+		;
+		
+	class_<aoflagger::Strategy>("Strategy", no_init);
+	
+	class_<aoflagger::AOFlagger, boost::noncopyable>("AOFlagger")
+		.def("make_image_set", MakeImageSet1)
+		.def("make_image_set", MakeImageSet2)
+		.def("make_image_set", MakeImageSet3)
+		.def("make_image_set", MakeImageSet4)
+		.def("make_flag_mask", MakeFlagMask1)
+		.def("make_flag_mask", MakeFlagMask2)
+		.def("make_strategy", MakeStrategy1)
+		.def("make_strategy", MakeStrategy2)
+		.def("make_strategy", MakeStrategy3)
+		.def("make_strategy", MakeStrategy4)
+		.def("make_strategy", MakeStrategy5)
+		.def("load_strategy", LoadStrategy)
+		.def("run", Run1)
+		.def("run", Run2)
+		.def("get_version_string", &aoflagger::AOFlagger::GetVersionString).staticmethod("get_version_string")
+		.def("get_version_date", &aoflagger::AOFlagger::GetVersionDate).staticmethod("get_version_date");
 }
